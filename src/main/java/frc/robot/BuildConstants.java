d1fbfe8037c3d4fbb14de62c36f2c882c3aa4214
NitinS6369 <nitin.s@mckinneysteamacademy.org>
2026-01-15 19:55:39 -0600

    replace tunerconstants warning, build updates

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,15 +3,15 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "FRC-CODE-TEMPLATES-6773-2026";
+  public static final String MAVEN_NAME = "FRC-CODE-TEMPLATES-6773";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = -1;
-  public static final String GIT_SHA = "UNKNOWN";
-  public static final String GIT_DATE = "UNKNOWN";
-  public static final String GIT_BRANCH = "UNKNOWN";
-  public static final String BUILD_DATE = "2026-01-15 20:20:04 EST";
-  public static final long BUILD_UNIX_TIME = 1768526404599L;
-  public static final int DIRTY = 129;
+  public static final int GIT_REVISION = 8;
+  public static final String GIT_SHA = "2e49e81e862c6cda2435e48484eff6a3c31d46f4";
+  public static final String GIT_DATE = "2026-01-15 20:50:23 EST";
+  public static final String GIT_BRANCH = "drivebase";
+  public static final String BUILD_DATE = "2026-01-15 20:51:23 EST";
+  public static final long BUILD_UNIX_TIME = 1768528283995L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
