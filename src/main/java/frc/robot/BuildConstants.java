ad5f406ec00b810ed065e8a13e48bc0be00b5f68
NitinS6369 <nitin.s@mckinneysteamacademy.org>
2026-01-19 16:09:52 -0600

    wpiLib update

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-FRC-6773";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 11;
-  public static final String GIT_SHA = "e85ad49bab08f827b95ebab0d218b8aef4db370f";
-  public static final String GIT_DATE = "2026-01-19 15:19:59 EST";
+  public static final int GIT_REVISION = 12;
+  public static final String GIT_SHA = "338b9efe3e455bcd8b90148db15c3f4161845f1a";
+  public static final String GIT_DATE = "2026-01-19 15:28:43 EST";
   public static final String GIT_BRANCH = "drivebase";
-  public static final String BUILD_DATE = "2026-01-19 15:22:32 EST";
-  public static final long BUILD_UNIX_TIME = 1768854152838L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2026-01-19 17:08:42 EST";
+  public static final long BUILD_UNIX_TIME = 1768860522834L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
