e85ad49bab08f827b95ebab0d218b8aef4db370f
NitinS6369 <nitin.s@mckinneysteamacademy.org>
2026-01-19 14:19:59 -0600

    rio2 optimization, vision integration

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,15 +3,15 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "FRC-CODE-TEMPLATES-6773";
+  public static final String MAVEN_NAME = "2026-FRC-6773";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 9;
-  public static final String GIT_SHA = "d1fbfe8037c3d4fbb14de62c36f2c882c3aa4214";
-  public static final String GIT_DATE = "2026-01-15 20:55:39 EST";
+  public static final int GIT_REVISION = 10;
+  public static final String GIT_SHA = "24ad63016f7f6ae2f4ce6121e5e063a75c91c265";
+  public static final String GIT_DATE = "2026-01-19 14:35:06 EST";
   public static final String GIT_BRANCH = "drivebase";
-  public static final String BUILD_DATE = "2026-01-15 21:04:39 EST";
-  public static final long BUILD_UNIX_TIME = 1768529079726L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-01-19 15:02:25 EST";
+  public static final long BUILD_UNIX_TIME = 1768852945585L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
