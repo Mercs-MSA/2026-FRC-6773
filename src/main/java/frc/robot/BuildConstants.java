e7e0421a35d7325854f1c873cb87bcafec74388c
Armaan Kakkar <Armaan.kakkar07@gmail.com>
2026-01-21 18:30:29 -0600

    working non tuned constants - AK

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-FRC-6773";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 12;
-  public static final String GIT_SHA = "338b9efe3e455bcd8b90148db15c3f4161845f1a";
-  public static final String GIT_DATE = "2026-01-19 15:28:43 EST";
+  public static final int GIT_REVISION = 13;
+  public static final String GIT_SHA = "ad5f406ec00b810ed065e8a13e48bc0be00b5f68";
+  public static final String GIT_DATE = "2026-01-19 17:09:52 EST";
   public static final String GIT_BRANCH = "drivebase";
-  public static final String BUILD_DATE = "2026-01-19 17:08:42 EST";
-  public static final long BUILD_UNIX_TIME = 1768860522834L;
+  public static final String BUILD_DATE = "2026-01-20 20:26:58 EST";
+  public static final long BUILD_UNIX_TIME = 1768958818168L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
