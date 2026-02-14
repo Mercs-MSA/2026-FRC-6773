13d53f9a215f2e4366a9d7ac2d10df495990d39c
JathinB <jathin.b@mckinneysteamacademy.org>
2026-02-14 17:38:09 -0600

    quick change of setvel for spindexer

--- src/main/java/frc/robot/subsystems/spindexer/Spindexer.java
+++ src/main/java/frc/robot/subsystems/spindexer/Spindexer.java
@@ -37,11 +37,7 @@ public class Spindexer extends SubsystemBase {
   }
 
   public void setVelocity(double velocity) {
-    constantVel = velocity;
-    kSpindexerHardware.setVelocity(-1 * velocity);
-    if (Constants.currentMode == Mode.SIM && constantVel != 0) {
-      constantVel = velocity;
-    }
+    kSpindexerHardware.setVelocity(velocity);
   }
 
   public void setVoltage(double voltage) {
