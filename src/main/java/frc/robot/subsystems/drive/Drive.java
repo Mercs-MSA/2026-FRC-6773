aec3a09b7d8298f4f5da3bcc8fdc887891a228fd
NitinS6773 <nitin.s@mckinneysteamacademy.org>
2026-02-19 16:49:35 -0600

    merge conflicts

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -217,7 +217,6 @@ public class Drive extends SubsystemBase {
     // Update gyro alert
     gyroDisconnectedAlert.set(!gyroInputs.connected && Constants.currentMode != Mode.SIM);
 
-<<<<<<< HEAD
     // Logger.recordOutput(
     //     "Odometry/FieldRelativeVelocity", RobotState.getInstance().getFieldVelocity());
 
@@ -225,15 +224,6 @@ public class Drive extends SubsystemBase {
     //     "Odometry/RobotStateEstimatedPose", RobotState.getInstance().getEstimatedPose());
 
     // Logger.recordOutput("Drive/DistanceToHub", RobotState.getInstance().distanceToHub());
-=======
-    Logger.recordOutput(
-        "Odometry/FieldRelativeVelocity", getFieldVelocity());
-
-    Logger.recordOutput(
-        "Odometry/RobotStateEstimatedPose", poseEstimator.getEstimatedPosition());
-
-    // Logger.recordOutput("Drive/DistanceToHub", );
->>>>>>> 64a7bb785e3c6486aef32c2e070abb523edd429b
   }
 
   /**
