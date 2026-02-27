d59bb3a4f7e2c60e6c400a62367a66605c319742
Armaan Kakkar <Armaan.kakkar07@gmail.com>
2026-02-26 18:49:54 -0600

    teleopo

--- src/main/java/frc/robot/commands/TeleopCommands.java
+++ src/main/java/frc/robot/commands/TeleopCommands.java
@@ -7,11 +7,7 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.constants.FieldConstants;
 import frc.robot.subsystems.drive.Drive;
-import frc.robot.subsystems.intake.Intake;
-import frc.robot.subsystems.intake.Intake.IntakeState;
 import frc.robot.subsystems.shooter.Shooter;
-import frc.robot.subsystems.spindexer.Spindexer;
-import frc.robot.subsystems.transfer.Transfer;
 import frc.robot.util.geometry.AllianceFlipUtil;
 
 public class TeleopCommands {
@@ -24,207 +20,17 @@ public class TeleopCommands {
 
   private CommandXboxController controller;
   private Drive drive;
-  private Intake intake;
-  private Spindexer mIndexer;
-  private Transfer mTransfer;
   private Shooter shooter;
 
   public TeleopCommands(
-      Intake intake,
-      Spindexer indexer,
-      Transfer transfer,
       Shooter shooter,
       Drive drive,
       CommandXboxController controller) {
-    this.intake = intake;
     this.controller = controller;
     this.shooter = shooter;
     this.drive = drive;
-    mIndexer = indexer;
-    mTransfer = transfer;
-    // kClimb = climb;
+  
   }
 
-  public Command runIntakeFloorPickup() {
-    return Commands.runOnce(
-        () -> {
-          intake.setPivotState(IntakeState.kFloorPickup);
-          intake.runRollers();
-          intake.setBrakeMode(false);
-        },
-        intake);
-  }
-
-  public Command runIntakeStow() {
-    return Commands.runOnce(
-        () -> {
-          intake.setPivotState(IntakeState.kStow);
-          intake.stowRollers();
-          intake.setBrakeMode(true);
-        },
-        intake);
-  }
-
-  public Command runIntakeSlowRollers() {
-    return Commands.run(
-        () -> {
-          // intake.setPivotState(IntakeState.kStow);
-          intake.slowRollers();
-          intake.setBrakeMode(true);
-        },
-        intake);
-  }
-
-  // public Command startShooting() {
-  //   return Commands.runOnce(
-  //       () -> {
-  //         mTransfer.startTransfer(12);
-  //       });
-  // }
-
-  // public Command whileShooting() {
-  //   return Commands.run(
-  //       () -> {
-  //         mIndexer.setState(mTransfer.getState());
-  //       });
-  // }
-
-  public Command stopShooting() {
-    return Commands.run(
-        () -> {
-          mTransfer.stopTransfer();
-          mIndexer.setState(ShooterState.INACTIVE);
-        });
-  }
-
-  public Command stopShoot() {
-    return Commands.runOnce(
-        () -> {
-          shooter.setFlywheelVelocityRPS(10);
-          // shooter.stop(true, false, false);
-          mTransfer.setRegulatorVelocity(10);
-        });
-  }
-
-  public Command startShoot() {
-    return Commands.run(
-        () -> {
-          // shooter.setFlywheelVelocityRPS(60);
-          mTransfer.setRegulatorVelocity(50);
-        });
-  }
-
-  public Command trackHub() {
-    if (drive.checkInAllianceZone(drive.getPose())) {
-      return shooter.runTrackTargetCommand();
-    } else {
-      return null;
-    }
-  }
-
-  public Command trackFlywheel() {
-    if (drive.checkInAllianceZone(drive.getPose())) {
-      return trackFlywheelHub();
-    } else {
-      return trackFlywheelPassRight();
-    }
-  }
-
-  public Command trackFlywheelHub() {
-    return shooter.runFlywheelTargetCommand(
-        AllianceFlipUtil.apply(FieldConstants.Hub.topCenterPoint));
-  }
-
-  public Command trackFlywheelPassRight() {
-    return shooter.runFlywheelTargetCommand(
-        new Translation3d(AllianceFlipUtil.apply(FieldConstants.RightBump.farRightCorner)));
-  }
-
-  public Command trackFlywheelPassLeft() {
-    return shooter.runFlywheelTargetCommand(
-        new Translation3d(AllianceFlipUtil.apply(FieldConstants.LeftBump.farLeftCorner)));
-  }
-
-  public Command idleShooter() {
-    return Commands.runOnce(
-        () -> {
-          shooter.setHoodPosition(Rotation2d.fromDegrees(1));
-          // shooter.setFlywheelVelocityRPS(10);
-          shooter.setTurretVoltage(0);
-          mTransfer.setRegulatorVelocity(10);
-        });
-  }
-
-  public Command startKick() {
-    return Commands.run(
-        () -> {
-          mTransfer.setKickerVelocity(40);
-          // TODO: ADD SHOOTER
-        });
-  }
-
-  public Command stopKick() {
-    return Commands.run(
-        () -> {
-          mTransfer.setKickerVelocity(0);
-          // TODO: ADD SHOOTER
-        });
-  }
-
-  public Command spin(double vel) {
-    return Commands.run(
-        () -> {
-          mIndexer.setVelocity(vel);
-        },
-        mIndexer);
-  }
-
-  public Command spinAlt() {
-    return Commands.run(
-        () -> {
-          mIndexer.setVelocity(-30);
-        },
-        mIndexer);
-  }
-
-  public Command spinStop() {
-    return Commands.run(
-        () -> {
-          mIndexer.setVelocity(0);
-        },
-        mIndexer);
-  }
-
-  public Command stop() {
-    return Commands.runOnce(
-        () -> {
-          mIndexer.stopSpindexer();
-        },
-        mIndexer);
-  }
-
-  public Command startTransfer(double shooterSpeed) {
-    return Commands.runOnce(
-        () -> {
-          mTransfer.startTransfer(shooterSpeed);
-        },
-        mTransfer);
-  }
-
-  public Command stopTransfer() {
-    return Commands.runOnce(
-        () -> {
-          mTransfer.stopTransfer();
-          mIndexer.setState(ShooterState.INACTIVE);
-        },
-        mTransfer);
-  }
-
-  public Command stopKicker() {
-    return Commands.runOnce(
-        () -> {
-          mTransfer.setKickerVoltage(0);
-        },
-        mTransfer);
-  }
+  
 }
