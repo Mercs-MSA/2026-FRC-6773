190993c96d1327a3821cea269119aab82a536f0c
JathinB <battepatijathin@gmail.com>
2026-03-23 18:15:13 -0500

    Fixed Depot Side
    Adding a little bit of shunting before shooting in order to unjam the hopper, then it intakes the shunted balls slowly

--- src/main/java/frc/robot/commands/AutonCommands.java
+++ src/main/java/frc/robot/commands/AutonCommands.java
@@ -8,7 +8,9 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.ChoreoFiles.ChoreoTraj;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.indexer.Indexer;
@@ -147,7 +149,7 @@ public class AutonCommands extends TeleopCommands {
                   drive.setPose(
                       AllianceFlipUtil.apply(ChoreoTraj.D_Partial_1Pass.initialPoseBlue()));
                 }));
-        autonCommand.addCommands(getAutonCommandSegments("D_Partial_1Pass"));
+        autonCommand.addCommands(getDepotSide());
         break;
       case "SHUNT_LEFT":
         String quick = "H_Shunt_Grab";
@@ -220,6 +222,32 @@ public class AutonCommands extends TeleopCommands {
     return command;
   }
 
+  public Command getDepotSide()
+  {
+    String quick = "D_Partial_1Pass";
+
+    SequentialCommandGroup command = new SequentialCommandGroup();
+
+    command.addCommands(getPathCommand(quick, 0));
+    command.addCommands(intakeCommand(IntakeState.INTAKING));
+
+    command.addCommands(getPathCommand(quick, 1));
+    command.addCommands(intakeCommand(IntakeState.IDLE));
+
+    command.addCommands(getPathCommand(quick, 2));
+    command.addCommands(stopDrive());            
+    command.addCommands(stopShootCommand());
+    Command com = Commands.parallel(intakeCommand(IntakeState.OUTTAKING), indexCommand(IndexerState.INDEXING), shootCommand());
+    command.addCommands(com);
+    command.addCommands(new WaitCommand(1));
+    command.addCommands(intakeCommand(IntakeState.INTAKING));
+    command.addCommands(getPathCommand(quick, 3));
+    command.addCommands(stopDrive());
+    // command.addCommands(new WaitCommand(1.5));
+    return command;
+  }
+
+
   public Command humanPlayerAuton(String name) {
     SequentialCommandGroup command = new SequentialCommandGroup();
 
@@ -287,5 +315,11 @@ public class AutonCommands extends TeleopCommands {
                 intake.setIntakeState(IntakeState.AGITATE);
               transfer.setTransferState(TransferState.TRANSFERRING);
             }));
+  }
+
+  public Command intakeCommand(IntakeState state, double seconds)
+  {
+    IntakeState original = intake.getIntakeState();
+    return intakeCommand(state).withTimeout(seconds).andThen(intakeCommand(original));
   }
 }
