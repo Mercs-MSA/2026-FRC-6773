896cb110a456c728285ad67df8c63686696c9581
Armaan Kakkar <Armaan.kakkar07@gmail.com>
2026-03-23 20:04:11 -0500

    auton changes

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-FRC-6773";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 124;
-  public static final String GIT_SHA = "9ac445b7f65dba5932a9d69db96a3c66c46a41c2";
-  public static final String GIT_DATE = "2026-03-23 16:11:01 EDT";
+  public static final int GIT_REVISION = 126;
+  public static final String GIT_SHA = "190993c96d1327a3821cea269119aab82a536f0c";
+  public static final String GIT_DATE = "2026-03-23 19:15:13 EDT";
   public static final String GIT_BRANCH = "rebuilt";
-  public static final String BUILD_DATE = "2026-03-23 17:10:08 EDT";
-  public static final long BUILD_UNIX_TIME = 1774300208751L;
+  public static final String BUILD_DATE = "2026-03-23 20:31:40 EDT";
+  public static final long BUILD_UNIX_TIME = 1774312300759L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
