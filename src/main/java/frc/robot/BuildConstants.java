255d5b4f2f675532fac7e47c399ddd0cc0f952ef
Armaan Kakkar <Armaan.kakkar07@gmail.com>
2026-03-28 17:46:44 -0500

    Increased Speed Over Bump

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-FRC-6773";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 150;
-  public static final String GIT_SHA = "0630323087b85e39ff8112e6abc1643e4d1211fb";
-  public static final String GIT_DATE = "2026-03-27 07:48:46 EDT";
+  public static final int GIT_REVISION = 151;
+  public static final String GIT_SHA = "50a4b1d5bdee65214bc5f0631c044775d282da49";
+  public static final String GIT_DATE = "2026-03-27 22:53:26 EDT";
   public static final String GIT_BRANCH = "rebuilt";
-  public static final String BUILD_DATE = "2026-03-27 21:59:31 EDT";
-  public static final long BUILD_UNIX_TIME = 1774663171553L;
+  public static final String BUILD_DATE = "2026-03-28 16:52:42 EDT";
+  public static final long BUILD_UNIX_TIME = 1774731162249L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
